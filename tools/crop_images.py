b0a8d850531c3cb34099889cd6d32c8934fad958
Nikos Michas <nikos.michas@orfium.com>
2021-06-11 20:18:53 +0300

    Update cropping script to work with non-existing folders

--- tools/crop_images.py
+++ tools/crop_images.py
@@ -4,15 +4,15 @@ from PIL import Image
 
 
 def crop_imgs_from_folder(in_folder, annot_folder, out_folder):
-    os.mkdir(out_folder)
+    os.makedirs(out_folder, exist_ok=True)
 
-    for dir in os.listdir(annot_folder):
-        os.mkdir(os.path.join(out_folder, dir))
-        print(dir)
+    for folder in os.listdir(annot_folder):
+        os.makedirs(os.path.join(out_folder, folder))
+        print(folder)
 
-        for x in os.listdir(os.path.join(annot_folder, dir)):
+        for x in os.listdir(os.path.join(annot_folder, folder)):
 
-            xml = os.path.join(annot_folder, dir, x)
+            xml = os.path.join(annot_folder, folder, x)
             with open(xml) as f:
                 s = BeautifulSoup(f)
                 xmin = int(s.annotation.object.bodybndbox.xmin.text)
@@ -20,7 +20,7 @@ def crop_imgs_from_folder(in_folder, annot_folder, out_folder):
                 ymin = int(s.annotation.object.bodybndbox.ymin.text)
                 ymax = int(s.annotation.object.bodybndbox.ymax.text)
 
-            img_file = os.path.join(in_folder, dir, x.replace('.xml', ''))
+            img_file = os.path.join(in_folder, folder, x.replace('.xml', ''))
 
             im = Image.open(img_file)
             im_cropped = im.crop((xmin, ymin, xmax, ymax))
@@ -28,8 +28,12 @@ def crop_imgs_from_folder(in_folder, annot_folder, out_folder):
             if im_cropped.mode in ("RGBA", "P"):
                 im_cropped = im_cropped.convert("RGB")
 
-            im_cropped.save(os.path.join(out_folder, dir, x.replace('.xml', '')))
+            im_cropped.save(os.path.join(out_folder, folder, x.replace('.xml', '')))
 
     return
 
 
+if __name__ == "__main__":
+    crop_imgs_from_folder('../data/high/high-resolution',
+                          '../data/high/high-annotations',
+                          '../data/high/high-resolution-cropped')
