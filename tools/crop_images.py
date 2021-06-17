a52755e6b2679b2c371a2f7ffab650b8571aafd7
Nikos Michas <nikos.michas@orfium.com>
2021-06-17 23:21:18 +0300

    Calculate crop bbox to be rectangle if possible

--- tools/crop_images.py
+++ tools/crop_images.py
@@ -23,7 +23,8 @@ def crop_imgs_from_folder(in_folder, annot_folder, out_folder):
             img_file = os.path.join(in_folder, folder, x.replace('.xml', ''))
 
             im = Image.open(img_file)
-            im_cropped = im.crop((xmin, ymin, xmax, ymax))
+            bbox = calculate_crop_bbox((xmin, ymin, xmax, ymax), im.size)
+            im_cropped = im.crop(bbox)
 
             if im_cropped.mode in ("RGBA", "P"):
                 im_cropped = im_cropped.convert("RGB")
@@ -33,7 +34,56 @@ def crop_imgs_from_folder(in_folder, annot_folder, out_folder):
     return
 
 
+def calculate_crop_bbox(dog_bbox: tuple, im_size: tuple) -> tuple:
+    """
+    Try to get a rectangular shape out of the image containing only the dog centered
+
+    :param dog_bbox: xmin, ymin, xmax, ymax of box body
+    :param im_size: width, height of image
+    :return: xmin, ymin, xmax, ymax to crop
+    """
+    xmin, ymin, xmax, ymax = dog_bbox
+    width = xmax - xmin
+    height = ymax - ymin
+    if width == height:
+        return dog_bbox
+    elif width < height:
+        diff = int((height - width) / 2)
+        new_xmin = xmin - diff
+        new_ymin = ymin
+        new_xmax = xmax + diff
+        new_ymax = ymax
+
+        if new_xmin < 0:
+            new_xmax = new_xmax - new_xmin
+            new_xmin = 0
+        elif new_xmax > im_size[0]:
+            new_xmin = new_xmin - (new_xmax - im_size[0])
+            new_xmax = im_size[0]
+
+        new_xmin = max(0, new_xmin)
+        new_xmax = min(im_size[0], new_xmax)
+    elif width > height:
+        diff = int((width - height) / 2)
+        new_xmin = xmin
+        new_ymin = ymin - diff
+        new_xmax = xmax
+        new_ymax = ymax + diff
+
+        if new_ymin < 0:
+            new_ymax = new_ymax - new_ymin
+            new_ymin = 0
+        elif new_ymax > im_size[1]:
+            new_ymin = new_ymin - (new_ymax - im_size[1])
+            new_ymax = im_size[1]
+
+        new_ymin = max(0, new_ymin)
+        new_ymax = min(im_size[1], new_ymax)
+
+    return new_xmin, new_ymin, new_xmax, new_ymax
+
+
 if __name__ == "__main__":
-    crop_imgs_from_folder('../data/high/high-resolution',
-                          '../data/high/high-annotations',
-                          '../data/high/high-resolution-cropped')
+    crop_imgs_from_folder('../data/low/low-resolution',
+                          '../data/low/low-annotations',
+                          '../data/low/low-resolution-cropped-2')
